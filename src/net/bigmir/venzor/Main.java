aa7cd143650b8e19591857fcfd0668e320f19867
AIgnatov <AIgnatov@DIZ-19.diz.ukrplastic.com>
2017-06-13 08:57:38 +0300

    FirstCommit

---
+++ src/net/bigmir/venzor/Main.java
@@ -0,0 +1,15 @@
+package net.bigmir.venzor;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Triangle triangleOne = new Triangle("один", 10, 18.6, 20);
+		triangleOne.output();
+		Triangle triangleTwo = new Triangle("два", 4, 5, 6);
+		triangleTwo.output();
+		Triangle triangleThree = new Triangle("три", 30, 10, 20);
+		triangleThree.output();
+
+	}
+
+}
---
+++ src/net/bigmir/venzor/Triangle.java
@@ -0,0 +1,83 @@
+package net.bigmir.venzor;
+
+public class Triangle {
+	private String name;
+	private double side1;
+	private double side2;
+	private double side3;
+	private double area = 0;
+
+	public Triangle(String name, double side1, double side2, double side3) {
+		super();
+		this.name = name;
+		this.side1 = side1;
+		this.side2 = side2;
+		this.side3 = side3;
+	}
+
+	public Triangle() {
+		super();
+	}
+
+	public double getSide1() {
+		return side1;
+	}
+
+	public void setSide1(double side1) {
+		this.side1 = side1;
+	}
+
+	public double getSide2() {
+		return side2;
+	}
+
+	public void setSide2(double side2) {
+		this.side2 = side2;
+	}
+
+	public double getSide3() {
+		return side3;
+	}
+
+	public void setSide3(double side3) {
+		this.side3 = side3;
+	}
+
+	public double getArea() {
+		return area;
+	}
+
+	public boolean exist() {
+		boolean f = false;
+		if ((side1 + side2) > side3 && (side1 + side3) > side2 && (side3 + side2) > side1) {
+			f = true;
+		}
+		return f;
+	}
+
+	public double area() {
+		double p = (side1 + side2 + side3) / 2;
+		area = Math.sqrt(p * (p - side1) * (p - side2) * (p - side3));
+		return area;
+	}
+
+	public void print() {
+		System.out.print("Стороны треугольника "+name+": ");
+		System.out.print(side1 + "; ");
+		System.out.print(side2 + "; ");
+		System.out.println(side3 + ".");
+
+	}
+
+	public void output() {
+		if (exist()) {
+			area();
+			print();
+			System.out.println("Площадь треугольника " + name + " = " + String.format("%.3f.", area));
+		} else {
+			print();
+			System.out.println("Такого треугольника не существует.");
+		}
+	}
+
+}
